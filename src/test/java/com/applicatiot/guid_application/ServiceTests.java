cd478c1c4a9d4c6c21a543b037c1fbfb62d6dec8
pjaro <dzejpi27@gmail.com>
2021-12-19 23:42:18 +0100

    Refactor imports

--- src/test/java/com/applicatiot/guid_application/ServiceTests.java
+++ src/test/java/com/applicatiot/guid_application/ServiceTests.java
@@ -5,19 +5,10 @@ import com.applicatiot.guid_application.domain.GuidDto;
 import com.applicatiot.guid_application.domain.ServiceGuid;
 import com.applicatiot.guid_application.external.GuidEntity;
 import com.applicatiot.guid_application.external.GuidRepository;
-import lombok.extern.slf4j.Slf4j;
-import lombok.var;
 import org.assertj.core.api.WithAssertions;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 
 import java.util.Optional;
 
